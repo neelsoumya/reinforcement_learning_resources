4953085d90724736962c07955d97daf069afba36
Soumya Banerjee <neel.soumya@gmail.com>
2025-03-08 09:30:31 +0000

    edits

---
+++ cartpole.py
@@ -0,0 +1,8 @@
+import gymnasium as gym
+env = gym.make('CartPole-v1')
+
+# TODO: https://aleksandarhaber.com/cart-pole-control-environment-in-openai-gym-gymnasium-introduction-to-openai-gym/
+# TODO: https://gymnasium.farama.org/introduction/basic_usage/
+# https://gym.openai.com/envs/CartPole-v1/
+# https://gym.openai.com/docs/
+
